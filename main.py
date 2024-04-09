402b9cab84bf8c98b666f72a14a340d7f963708a
Francesco Mazzitelli <pannemar2@gmail.com>
2024-04-09 14:24:30 +0200

    v0.2

--- main.py
+++ main.py
@@ -7,15 +7,28 @@ import threading
 import sys
 import re
 
+def choose_unique_representation(strings):
+    return max(strings, key=len)
+
+def find_similar_words(input_string, string_list, threshold):
+    similar_words = [input_string]  # Aggiungiamo la stringa di input alla lista
+    for word in string_list:
+        similarity_ratio = difflib.SequenceMatcher(None, input_string, word).ratio()
+        if similarity_ratio >= threshold:
+            similar_words.append(word)
+    return similar_words
+
 def compute_similarity(args):
     stringa1, stringa2 = args
     matcher = difflib.SequenceMatcher(None, stringa1, stringa2)
     similarita = matcher.ratio()
     return similarita
 
-def create_similarity_matrix(lista1, lista2):
+def create_similarity_matrix(lista1, lista2, progress_label, progress_var):
     num_stringhe1 = len(lista1)
     num_stringhe2 = len(lista2)
+    current_iteration = 0
+    total_iterations = num_stringhe1 * num_stringhe2
 
     # Inizializza la matrice di similarità con zeri
     matrice_similarita = np.zeros((num_stringhe1, num_stringhe2))
@@ -24,6 +37,10 @@ def create_similarity_matrix(lista1, lista2):
     for i in range(num_stringhe1):
         for j in range(num_stringhe2):
             matrice_similarita[i, j] = compute_similarity((lista1[i], lista2[j]))
+            current_iteration += 1
+            progress = (current_iteration / total_iterations) * 100
+            progress_var.set(progress)
+            root.update()  # Aggiornamento della GUI
 
     return matrice_similarita
 
@@ -62,43 +79,66 @@ def browse_shapefile(entry_var):
     filename = filedialog.askopenfilename(filetypes=[("Shapefile", "*.shx")])
     entry_var.set(filename)
 
-def process_data(file_path, shapefile_path, root, progress_var):
+def process_data(file_path, shapefile_path, root, progress_label, progress_var):
     file = pd.read_excel(file_path)
-    progress_var.set(2)
 
     shp = gpd.read_file(shapefile_path)
-    progress_var.set(4)
 
     lista_strade_da_bonficare = file['Etichette di riga']
     lista_strade_osm = shp['name']
-    progress_var.set(8)
 
     list1 = lista_strade_osm.tolist()
     list2 = lista_strade_da_bonficare.tolist()
-    progress_var.set(10)
     
+    progress_label.config(text="Preprocessing lista strade osm...")
     list1_p = preprocess(list1)
+    progress_var.set(0)
+    progress_label.config(text="Preprocessing lista strade excel...")
     list2_p = preprocess(list2)
-    progress_var.set(20)
+    progress_var.set(0)
 
-    matrice_similarita = create_similarity_matrix(list1_p, list2_p)
-    progress_var.set(40)
+    progress_label.config(text="Creazione matrice di similarità...")
+    matrice_similarita = create_similarity_matrix(list1_p, list2_p, progress_label, progress_var)
+    progress_var.set(0)
 
     list1_index = [string for string in list1 if string is not None]
 
     df = pd.DataFrame(matrice_similarita, index=list1_index, columns=list2)
     #df.to_excel("similarity_matrix.xlsx")
 
-    progress_var.set(70)
-
     # Trova l'indice del valore massimo solo se è maggiore di 0.85
     max_indices = df.apply(lambda col: col.idxmax() if col.max() > 0.85 else "", axis=0)
-    progress_var.set(80)
 
     # Aggiungi i valori massimi alla colonna "JOIN" nel file iniziale
     file['JOIN'] = max_indices.values
+
+    # Lista delle stringhe che non hanno valore nella colonna "JOIN"
+    empty_join_strings = np.array(file[file['JOIN'] == ""]['Etichette di riga'])
+
+    progress_label.config(text="Postprocessing...")
+    total_iterations = len(empty_join_strings)
+    current_iteration = 0
+    
+    for string in empty_join_strings:
+        if string != "":
+            # Trova tutte le stringhe simili alla stringa corrente
+            similar_strings = find_similar_words(string, empty_join_strings, 0.85)
+
+            if len(similar_strings) > 1:
+                # Trova una rappresentazione unica per il gruppo di stringhe simili
+                unique_representation = choose_unique_representation(similar_strings)
+
+                # Assegna la rappresentazione unica a tutte le stringhe simili
+                file.loc[file['Etichette di riga'].isin(similar_strings), 'MOST SIGNIFICANT'] = unique_representation
+
+            current_iteration += 1
+            progress = (current_iteration / total_iterations) * 100
+            progress_var.set(progress)
+            root.update()  # Aggiornamento della GUI
+
     progress_var.set(85)
 
+    progress_label.config(text="Finalizzazione...")
     # Salva il file aggiornato
     file.to_excel("TARI_con_JOIN.xlsx")
     progress_var.set(100)
@@ -109,21 +149,10 @@ def process_data(file_path, shapefile_path, root, progress_var):
     # Resetta la barra di avanzamento
     progress_var.set(0)
 
-class ProcessingThread(threading.Thread):
-    def __init__(self, file_path_var, shapefile_path_var, root, progress_var):
-        threading.Thread.__init__(self)
-        self.file_path = file_path_var.get()
-        self.shapefile_path = shapefile_path_var.get()
-        self.root = root
-        self.progress_var = progress_var
-
-    def run(self):
-        process_data(self.file_path, self.shapefile_path, self.root, self.progress_var)
-
 if __name__ == "__main__":
     root = Tk()
     root.title("Seleziona File")
-    root.geometry("500x250")  # Imposta le dimensioni della finestra
+    root.geometry("500x300")  # Aggiornato per dare spazio alla label sopra la progress bar
 
     file_path_var = StringVar()
     shapefile_path_var = StringVar()
@@ -150,11 +179,14 @@ if __name__ == "__main__":
     progress_bar = ttk.Progressbar(root, variable=progress_var, length=400, mode="determinate")
     progress_bar.grid(row=2, column=0, columnspan=3, pady=20, padx=10)
 
-    ok_button = Button(root, text="OK", command=lambda: ProcessingThread(file_path_var, shapefile_path_var, root, progress_var).start())
-    ok_button.grid(row=3, column=1, pady=20)
+    progress_label = Label(root, text="")
+    progress_label.grid(row=3, column=0, columnspan=3, pady=10, padx=10)  # Posizionato sopra la progress bar
+
+    ok_button = Button(root, text="OK", command=lambda: threading.Thread(target=process_data, args=(file_path_var.get(), shapefile_path_var.get(), root, progress_label, progress_var)).start())
+    ok_button.grid(row=4, column=1, pady=20)
 
     cancel_button = Button(root, text="Annulla", command=lambda: sys.exit(0))  # Chiudi il programma quando si clicca su "Annulla"
-    cancel_button.grid(row=3, column=2, pady=20)
+    cancel_button.grid(row=4, column=2, pady=20)
 
     root.protocol("WM_DELETE_WINDOW", lambda: sys.exit(0))  # Chiudi il programma quando si clicca sulla "X"
     root.mainloop()
